3eb2d1117ed62559a06a8f84804f9029b7f3e653
hoangnd17 <dumuc2010@gmail.com>
2022-10-17 22:42:41 +0700

    Updated node environment

--- fresh.sh
+++ fresh.sh
@@ -39,7 +39,10 @@ if test $(pyenv-virtualenv); then
 fi
 
 # Setup node environment
-if test $(which nvm); then
+if test ! $(which nvm); then
+  echo "$(curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash)"
+  export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
   eval "$(nvm install --lts)";
 fi
 
