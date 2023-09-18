232b61f9ead20f6a76dabb6d8528008c0958c843
hoangnd17 <dumuc2010@gmail.com>
2023-09-18 18:39:37 +0700

    Format

--- fresh.sh
+++ fresh.sh
@@ -4,15 +4,15 @@ echo "Setting up your Mac..."
 
 # Check for Oh My Zsh and install if we don't have it
 if test ! $(which omz); then
-   /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
+  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
 fi
 
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
-   echo "installing brew"
-   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /$HOME/.zprofile
-   eval "$(/opt/homebrew/bin/brew shellenv)"
+  echo "installing brew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >>/$HOME/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 # Install zplug
@@ -36,28 +36,28 @@ mkdir $HOME/Code
 
 # Setup python environment
 if test $(which pyenv); then
-  eval "$(pyenv init -)";
-  eval "$(pyenv install 3.9.14)";
-  eval "$(pyenv global 3.9.14)";
+  eval "$(pyenv init -)"
+  eval "$(pyenv install 3.9.14)"
+  eval "$(pyenv global 3.9.14)"
 fi
 
 if test $(which pyenv-virtualenv); then
-   eval "$(pyenv virtualenv-init -)";
+  eval "$(pyenv virtualenv-init -)"
 fi
 
 # Setup node environment
 if test ! $(which nvm); then
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
-  export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")" 
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
+  export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
   nvm install --lts
 fi
 
 # Setup ruby environment
 if test $(which rbenv); then
-  eval "$(rbenv init -)";
-  eval "$(rbenv install 3.1.2)";
-  eval "$(rbenv global 3.1.2)";
+  eval "$(rbenv init -)"
+  eval "$(rbenv install 3.1.2)"
+  eval "$(rbenv global 3.1.2)"
 fi
 
 # Make ZSH the default shell environment
--- ssh.sh
+++ ssh.sh
@@ -15,7 +15,7 @@ echo "Host *\n AddKeysToAgent yes\n UseKeychain yes\n IdentityFile ~/.ssh/id_ed2
 
 ssh-add -K ~/.ssh/id_ed25519
 # https://gist.github.com/vikpe/34454d69fe03a9617f2b009cc3ba200b
-ssh-keyscan github.com >> ~/.ssh/known_hosts
+ssh-keyscan github.com >>~/.ssh/known_hosts
 
 # Adding your SSH key to your GitHub account
 # https://docs.github.com/en/github/authenticating-to-github/adding-a-new-ssh-key-to-your-github-account
